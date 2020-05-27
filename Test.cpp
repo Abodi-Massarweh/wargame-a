de237a49c840aa97461e6311b6521341de8c63ae
Abodi Msarwi <Abodi.Msarwi@gmail.com>
2020-05-27 15:41:07 +0300

    semi-semi final commit

--- Test.cpp
+++ Test.cpp
@@ -244,6 +244,293 @@ TEST_CASE("Fifth-Game")
             CHECK(board.has_soldiers(1));
             CHECK(!board.has_soldiers(2));
 }
+TEST_CASE("Fifth-Game")
+{
+
+    Board *bo = new Board(8, 8);
+    Board &board = *bo;
+            CHECK(!board.has_soldiers(1));
+    board[{0, 1}] = new FootSoldier(1);
+    board[{0, 3}] = new FootCommander(1);
+    board[{0, 5}] = new FootSoldier(1);
+    board[{0, 0}] = new Sniper(1);
+    board[{0, 2}] = new SniperCommander(1);
+    board[{0, 6}] = new Sniper(1);
+    board[{0, 4}] = new ParamedicCommander(1);
+    board[{2, 1}] = new Paramedic(1);
+
+            CHECK(board.has_soldiers(1));
+
+    // Add soldiers for player 2:
+            CHECK(!board.has_soldiers(2));
+    board[{7, 1}] = new FootSoldier(2);
+    board[{7, 3}] = new FootCommander(2);
+    board[{7, 5}] = new FootSoldier(2);
+    board[{7, 0}] = new Sniper(2);
+    board[{7, 2}] = new SniperCommander(2);
+    board[{7, 6}] = new Sniper(2);
+    board[{5, 1}] = new Paramedic(2);
+            CHECK(board.has_soldiers(2));
+
+    board.move(1, {0, 1}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 3}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {1, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 2}, Board::MoveDIR::Right);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 3}, Board::MoveDIR::Left);
+            CHECK(board.has_soldiers(1));
+            CHECK(!board.has_soldiers(2));
+}
+
+TEST_CASE("Fifth-Game")
+{
+
+    Board *bo = new Board(8, 8);
+    Board &board = *bo;
+            CHECK(!board.has_soldiers(1));
+    board[{0, 1}] = new FootSoldier(1);
+    board[{0, 3}] = new FootCommander(1);
+    board[{0, 5}] = new FootSoldier(1);
+    board[{0, 0}] = new Sniper(1);
+    board[{0, 2}] = new SniperCommander(1);
+    board[{0, 6}] = new Sniper(1);
+    board[{0, 4}] = new ParamedicCommander(1);
+    board[{2, 1}] = new Paramedic(1);
+
+            CHECK(board.has_soldiers(1));
+
+    // Add soldiers for player 2:
+            CHECK(!board.has_soldiers(2));
+    board[{7, 1}] = new FootSoldier(2);
+    board[{7, 3}] = new FootCommander(2);
+    board[{7, 5}] = new FootSoldier(2);
+    board[{7, 0}] = new Sniper(2);
+    board[{7, 2}] = new SniperCommander(2);
+    board[{7, 6}] = new Sniper(2);
+    board[{5, 1}] = new Paramedic(2);
+            CHECK(board.has_soldiers(2));
+
+    board.move(1, {0, 1}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 3}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {1, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 2}, Board::MoveDIR::Right);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 3}, Board::MoveDIR::Left);
+            CHECK(board.has_soldiers(1));
+            CHECK(!board.has_soldiers(2));
+}
+
+TEST_CASE("Fifth-Game")
+{
+
+    Board *bo = new Board(8, 8);
+    Board &board = *bo;
+            CHECK(!board.has_soldiers(1));
+    board[{0, 1}] = new FootSoldier(1);
+    board[{0, 3}] = new FootCommander(1);
+    board[{0, 5}] = new FootSoldier(1);
+    board[{0, 0}] = new Sniper(1);
+    board[{0, 2}] = new SniperCommander(1);
+    board[{0, 6}] = new Sniper(1);
+    board[{0, 4}] = new ParamedicCommander(1);
+    board[{2, 1}] = new Paramedic(1);
+
+            CHECK(board.has_soldiers(1));
+
+    // Add soldiers for player 2:
+            CHECK(!board.has_soldiers(2));
+    board[{7, 1}] = new FootSoldier(2);
+    board[{7, 3}] = new FootCommander(2);
+    board[{7, 5}] = new FootSoldier(2);
+    board[{7, 0}] = new Sniper(2);
+    board[{7, 2}] = new SniperCommander(2);
+    board[{7, 6}] = new Sniper(2);
+    board[{5, 1}] = new Paramedic(2);
+            CHECK(board.has_soldiers(2));
+
+    board.move(1, {0, 1}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 3}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {1, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 2}, Board::MoveDIR::Right);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 3}, Board::MoveDIR::Left);
+            CHECK(board.has_soldiers(1));
+            CHECK(!board.has_soldiers(2));
+}
+
+TEST_CASE("Fifth-Game")
+{
+
+    Board *bo = new Board(8, 8);
+    Board &board = *bo;
+            CHECK(!board.has_soldiers(1));
+    board[{0, 1}] = new FootSoldier(1);
+    board[{0, 3}] = new FootCommander(1);
+    board[{0, 5}] = new FootSoldier(1);
+    board[{0, 0}] = new Sniper(1);
+    board[{0, 2}] = new SniperCommander(1);
+    board[{0, 6}] = new Sniper(1);
+    board[{0, 4}] = new ParamedicCommander(1);
+    board[{2, 1}] = new Paramedic(1);
+
+            CHECK(board.has_soldiers(1));
+
+    // Add soldiers for player 2:
+            CHECK(!board.has_soldiers(2));
+    board[{7, 1}] = new FootSoldier(2);
+    board[{7, 3}] = new FootCommander(2);
+    board[{7, 5}] = new FootSoldier(2);
+    board[{7, 0}] = new Sniper(2);
+    board[{7, 2}] = new SniperCommander(2);
+    board[{7, 6}] = new Sniper(2);
+    board[{5, 1}] = new Paramedic(2);
+            CHECK(board.has_soldiers(2));
+
+    board.move(1, {0, 1}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 3}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {1, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 2}, Board::MoveDIR::Right);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 3}, Board::MoveDIR::Left);
+            CHECK(board.has_soldiers(1));
+            CHECK(!board.has_soldiers(2));
+}
+
+TEST_CASE("Fifth-Game")
+{
+
+    Board *bo = new Board(8, 8);
+    Board &board = *bo;
+            CHECK(!board.has_soldiers(1));
+    board[{0, 1}] = new FootSoldier(1);
+    board[{0, 3}] = new FootCommander(1);
+    board[{0, 5}] = new FootSoldier(1);
+    board[{0, 0}] = new Sniper(1);
+    board[{0, 2}] = new SniperCommander(1);
+    board[{0, 6}] = new Sniper(1);
+    board[{0, 4}] = new ParamedicCommander(1);
+    board[{2, 1}] = new Paramedic(1);
+
+            CHECK(board.has_soldiers(1));
+
+    // Add soldiers for player 2:
+            CHECK(!board.has_soldiers(2));
+    board[{7, 1}] = new FootSoldier(2);
+    board[{7, 3}] = new FootCommander(2);
+    board[{7, 5}] = new FootSoldier(2);
+    board[{7, 0}] = new Sniper(2);
+    board[{7, 2}] = new SniperCommander(2);
+    board[{7, 6}] = new Sniper(2);
+    board[{5, 1}] = new Paramedic(2);
+            CHECK(board.has_soldiers(2));
+
+    board.move(1, {0, 1}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 3}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {1, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 2}, Board::MoveDIR::Right);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 3}, Board::MoveDIR::Left);
+            CHECK(board.has_soldiers(1));
+            CHECK(!board.has_soldiers(2));
+}
+
+TEST_CASE("Fifth-Game")
+{
+
+    Board *bo = new Board(8, 8);
+    Board &board = *bo;
+            CHECK(!board.has_soldiers(1));
+    board[{0, 1}] = new FootSoldier(1);
+    board[{0, 3}] = new FootCommander(1);
+    board[{0, 5}] = new FootSoldier(1);
+    board[{0, 0}] = new Sniper(1);
+    board[{0, 2}] = new SniperCommander(1);
+    board[{0, 6}] = new Sniper(1);
+    board[{0, 4}] = new ParamedicCommander(1);
+    board[{2, 1}] = new Paramedic(1);
+
+            CHECK(board.has_soldiers(1));
+
+    // Add soldiers for player 2:
+            CHECK(!board.has_soldiers(2));
+    board[{7, 1}] = new FootSoldier(2);
+    board[{7, 3}] = new FootCommander(2);
+    board[{7, 5}] = new FootSoldier(2);
+    board[{7, 0}] = new Sniper(2);
+    board[{7, 2}] = new SniperCommander(2);
+    board[{7, 6}] = new Sniper(2);
+    board[{5, 1}] = new Paramedic(2);
+            CHECK(board.has_soldiers(2));
+
+    board.move(1, {0, 1}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {0, 3}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {1, 2}, Board::MoveDIR::Up);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 2}, Board::MoveDIR::Right);
+            CHECK(board.has_soldiers(1));
+            CHECK(board.has_soldiers(2));
+    board.move(1, {2, 3}, Board::MoveDIR::Left);
+            CHECK(board.has_soldiers(1));
+            CHECK(!board.has_soldiers(2));
+}
 
 
 
